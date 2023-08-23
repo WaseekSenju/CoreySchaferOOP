8d64d3f84bca0a6dab2dca77a139a31c65ab18fe
WaseekSenju <waseeq69ahmed@gmail.com>
2023-08-23 12:07:11 +0500

    First Lecture

--- Classes and Instances.py
+++ Classes and Instances.py
@@ -5,9 +5,15 @@ class Person:
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
 
 p1 = Person('Waseeeq', 'Ahmed', 100000)
 p2 = Person('Rohan', 'Bashir', 2000)
 
-print(p1)
-print(p2)
+print(p1.fullname())
+print(p2.fullname())
+
+
+
